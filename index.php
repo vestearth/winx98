de378c5bcc494e9c34aee6204ee91b9cded083a1
sk-earth <sichol.kor@gmail.com>
2025-05-20 21:10:42 +0700

    feat: move alliance data retrieval inside login check for better user context

--- index.php
+++ index.php
@@ -9,8 +9,6 @@ $options = [
   'on_time' => true,
 ];
 $runnertext = nga_management::getRunnerText($code, $options);
-$user_data = User::getCurrent();
-$alliance_data = nga_management::getAllianceByID($code, $user_data['alliance_id']);
 $this_page = 'index';
 ?>
 
@@ -30,6 +28,7 @@ $this_page = 'index';
   <?php
   if ($is_login) {
     $user_current = User::getCurrent();
+    $alliance_data = nga_management::getAllianceByID($code, $user_current['alliance_id']);
     $data = [
       'user_id' => $user_current['id'],
       'detail' => 'เข้าหน้าแรก',
